4a9948afb8e6dd91de78441020300b292c07f1aa
Milton <milton.hneto@gmail.com>
2022-03-28 20:59:28 -0300

    ProductDetails loaders

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Product } from "types/product";
 import { BASE_URL } from "util/requests";
+import ProductDetailsLoader from "./ProductDetailsLoader";
+import ProductInfoLoader from "./ProductInfoLoader";
 
 import './styles.css';
 
@@ -16,13 +18,16 @@ type UrlParams = {
 const ProductDetails = () => {
 
   const { productId } = useParams<UrlParams>();
-
+  const [isLoading, setIsLoading] = useState(false);
   const [product, setProduct] = useState<Product>();
 
   useEffect(() => {
+    setIsLoading(true);
     axios.get(`${BASE_URL}/products/${productId}`)
     .then(response => {
       setProduct(response.data);
+    }).finally(() => {
+      setIsLoading(false);
     });
   }, [productId])
 
@@ -37,19 +42,25 @@ const ProductDetails = () => {
         </Link>
         <div className="row">
           <div className="col-xl-6">
-            <div className="img-container">
-              <img src={product?.imgUrl} alt={product?.name} />
-            </div>
-            <div className="name-price-container">
-              <h1>{product?.name}</h1>
-              {product && <ProductPrice price={product?.price} />}
-            </div>
+            {isLoading ? (<ProductInfoLoader />) : (
+              <>
+                <div className="img-container">
+                  <img src={product?.imgUrl} alt={product?.name} />
+                </div>
+                <div className="name-price-container">
+                  <h1>{product?.name}</h1>
+                  {product && <ProductPrice price={product?.price} />}
+                </div>
+              </>
+            )}
           </div>
           <div className="col-xl-6">
-            <div className="description-container">
+            {isLoading ? (<ProductDetailsLoader />) : (
+              <div className="description-container">
               <h2>Descrição do produto</h2>
               <p>{product?.description}</p>
-            </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
